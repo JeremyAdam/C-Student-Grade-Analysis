c203774030a333e15fb65213db10a0703ee99ced
JeremyAdam <83097265+JeremyAdam@users.noreply.github.com>
2022-02-11 09:49:49 -0700

    Add files via upload

--- C# - Student Grade Analysis/Program.cs
+++ C# - Student Grade Analysis/Program.cs
@@ -1,48 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
-namespace Test
-{
-    class studentGrades {
-        static void Main(string[] agrs)
+class studentGrades {
+    static void Main()
+    {
+        // Program Variables
+        Random random = new Random(); // Helps Create The Random Int
+        List<int> gradesList = new List<int>(); // Creates List
+        int select = 0; // For Selcting Things From Menu
+        bool run = true; // For Running The Program
+        
+        // Generates 50 Random Grades
+        for (int i = 0; i < 50; i++)
         {
-            Random random = new Random(); // Helps Create The Random Int
-            List<int> gradesList = new List<int>(); // Creates List
-            int select = 0; // For Selcting Things From Menu
-            bool menuOpen = true; // For Loading The Main Menu
-            bool run = true; // For Running The Program
-            for (int i = 0; i < 50; i++) // Generates 50 Random Grades
+            int randNum = random.Next(0, 100);
+            gradesList.Add(randNum);
+        }
+
+        // Main Loop
+        while (run == true)
+        {
+            // Creates The Main Menu
+            Console.WriteLine(" ");                
+            Console.WriteLine("STUDENT GRADES");
+            Console.WriteLine("  1: Display All Grades.");
+            Console.WriteLine("  2: Randomize Grades.");
+            Console.WriteLine("  3: Stats.");
+            Console.WriteLine("  4: Count Honours.");
+            Console.WriteLine("  5: Exit");
+            Console.Write("SELECT (1-5): ");
+            select = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");      
+            switch(select)
             {
-                int randNum = random.Next(0, 100);
-                gradesList.Add(randNum);
-            }
-            while (run == true)
-            { // Runs Program
-                while (menuOpen == true)
-                { // Creates The Main Menu
-                    Console.WriteLine(" ");                
-                    Console.WriteLine("STUDENT GRADES");
-                    Console.WriteLine("  1: Display All Grades.");
-                    Console.WriteLine("  2: Randomize Grades.");
-                    Console.WriteLine("  3: Stats.");
-                    Console.WriteLine("  4: Count Honours.");
-                    Console.WriteLine("  5: Exit");
-                    Console.WriteLine("SELECT (1-5): ");
-                    select = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(" ");      
-                    menuOpen = false;
-                }
-                if (select == 1)
-                { // Display All Grades
-                    foreach (int a in gradesList)
+                case 1: // Display All Grades
+                    foreach (int grade in gradesList)
                     {
-                        Console.WriteLine(a);
+                        Console.WriteLine(grade);
                     }
-                    select = 0;
-                    menuOpen = true;
-                } else if (select == 2)
-                { // Randomize Grades
+                    select = 0;                    
+                    break;
+                 case 2: // Randomize Grades
                     gradesList.Clear();
                     for (int i = 0; i < 50; i++)
                     {
@@ -50,18 +48,16 @@ namespace Test
                         gradesList.Add(randNum);
                     }
                     Console.WriteLine("Grades randomized");
-                    select = 0;
-                    menuOpen = true;
-                } else if (select == 3)
-                { // Generate General Statistics
+                    select = 0;                
+                    break;
+                case 3: // Generate General Statistics
                     int [] gradesArray = gradesList.ToArray();
                     Console.WriteLine("The highest grade was: " + gradesArray.Max());
                     Console.WriteLine("The lowest grade was: " + gradesArray.Min());
                     Console.WriteLine("The average grade was: " + gradesArray.Sum() / gradesArray.Length);                    
-                    select = 0;
-                    menuOpen = true;
-                } else if (select == 4)
-                { // Show All Honour Grades
+                    select = 0;                    
+                    break;
+                case 4: // Show All Honour Grades
                     int honourAmount = 0;
                     for (int i = 0; i < gradesList.Count; i++)
                     {
@@ -71,19 +67,17 @@ namespace Test
                             honourAmount++;
                         }
                     }
-                    Console.WriteLine("There were ( " + Convert.ToString(honourAmount) + " ) honour students.");
+                    Console.WriteLine("There were (" + Convert.ToString(honourAmount) + ") honour students.");
                     select = 0;
-                    menuOpen = true;
-                } else if (select == 5)
-                { // End Program
-                    run = false;
-                } else
-                {
+                    break;
+                case 5: // End Program
+                    run = false;                
+                    break;
+                default:
                     Console.WriteLine("Value does not work");
-                    select = 0;
-                    menuOpen = true;
-                }
-            } 
-        }
+                    select = 0;                
+                    break;
+            }
+        } 
     }
 }
